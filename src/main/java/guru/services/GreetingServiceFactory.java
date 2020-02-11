1b6b1d02ea58cc1bec2a14ef5c2429b01d384634
Heindrich Paul <heindrich.paul@gmail.com>
2020-02-11 15:35:54 +0100

    Added factory

---
+++ src/main/java/guru/services/GreetingServiceFactory.java
@@ -0,0 +1,25 @@
+package guru.services;
+
+/**
+ * Created by heindrichpaul on 11/02/2020
+ */
+
+public class GreetingServiceFactory {
+    private GreetingRepository greetingRepository;
+
+    public GreetingServiceFactory(GreetingRepository greetingRepository) {
+        this.greetingRepository = greetingRepository;
+    }
+
+    public GreetingService createGreetingService(String lang) {
+        switch (lang) {
+            case "es":
+                return new PrimarySpanishGreetingService(greetingRepository);
+            case "de":
+                return new PrimaryGermanGreetingService(greetingRepository);
+            default:
+                return new PrimaryGreetingService(greetingRepository);
+        }
+
+    }
+}
